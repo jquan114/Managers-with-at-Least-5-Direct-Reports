1a9f806019ec42f232c94cb7b43a1d02f9af0ad0
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-07-19 18:02:57 -0400

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,27 @@
+-- Table: Employee
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | department  | varchar |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- In SQL, id is the primary key column for this table.
+-- Each row of this table indicates the name of an employee, their department, and the id of their manager.
+-- If managerId is null, then the employee does not have a manager.
+-- No employee will be the manager of themself.
+ 
+
+-- Find the managers with at least five direct reports.
+
+-- Return the result table in any order.
+
+-- # Write your MySQL query statement below
+
+SELECT e1.name
+FROM Employee e1
+JOIN Employee e2 ON e1.id = e2.managerId
+GROUP BY e1.id, e1.name
+HAVING COUNT(e2.id) >= 5;
